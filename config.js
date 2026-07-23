e181b49cab0f513afd5dec6d51d3f462259ea382
exam-ui <msbte.exam.0024@gmail.com>
2026-07-23 09:29:41 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// ------------------------------------------------------------------
+// EDIT THIS FILE ONLY: paste the Web App URL you get after deploying
+// Code.gs in Google Apps Script (see README.md, Step 3).
+// It looks like: https://script.google.com/macros/s/AKfycb.../exec
+// ------------------------------------------------------------------
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwB6z3wpx6qGmljZGvxXHBulWV99Z1_cnt7DlGSUbt6qypzxUAoHrPanM-b42716It-tw/exec';
