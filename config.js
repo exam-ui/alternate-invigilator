d0a617a393e179209c6f687a37c152543c5e957d
exam-ui <msbte.exam.0024@gmail.com>
2026-07-23 12:58:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // EDIT THIS FILE ONLY: paste the Web App URL you get after deploying
 // Code.gs in Google Apps Script (see README.md, Step 3).
 // It looks like: https://script.google.com/macros/s/AKfycb.../exec
-// ------------------------------------------------------------------
+// -----------------------------------------------------------------
 const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxGtQMlbEJDO3leX5dI_UG4tZeM6ocPKeYHqgAzTQxzgjA1j0JO_z1GaJ-gVUha9Qvt/exec';
